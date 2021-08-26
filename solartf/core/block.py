c2e1d40c9b2a0eb15f5374c9b3043c317d31dee0
黃上瑜 <shangyuhuang@gmail.com>
2021-08-26 17:00:24 +0800

    FEATURE: add a resnetv2 based keypoint detector

--- solartf/core/block.py
+++ solartf/core/block.py
@@ -1,9 +1,55 @@
 from tensorflow.keras import backend as K
-from tensorflow.keras.layers import (Add, BatchNormalization, Conv2D, DepthwiseConv2D,
-                                     GlobalAveragePooling2D, Multiply, ReLU, Reshape,
-                                     ZeroPadding2D)
+from tensorflow.keras.layers import (Add, Activation, BatchNormalization, Conv2D,
+                                     DepthwiseConv2D, GlobalAveragePooling2D, Multiply,
+                                     ReLU, Reshape, ZeroPadding2D)
+from tensorflow.keras.regularizers import l2
 from .activation import hard_sigmoid
 from .util import (correct_pad, get_filter_nb_by_depth)
+
+
+def resnet_block(inputs,
+                 num_filters=16,
+                 kernel_size=3,
+                 strides=1,
+                 activation='relu',
+                 batch_normalization=True,
+                 conv_first=True):
+    """2D Convolution-Batch Normalization-Activation stack builder
+
+    # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+
+    # Returns
+        x (tensor): tensor as input to the next layer
+    """
+    conv = Conv2D(num_filters,
+                  kernel_size=kernel_size,
+                  strides=strides,
+                  padding='same',
+                  kernel_initializer='he_normal',
+                  kernel_regularizer=l2(1e-4))
+
+    x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
 
 
 def inverted_res_block(x,
---
+++ solartf/kptdetector/model.py
@@ -0,0 +1,60 @@
+from tensorflow.keras.layers import (Concatenate, Dense, Dropout, GlobalAveragePooling2D, Input, Reshape)
+from tensorflow.keras.losses import (binary_crossentropy, mse)
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from solartf.core.model import TFModelBase
+from solartf.core.graph import ResNetV2
+
+
+class TFResNet(TFModelBase):
+    def __init__(self,
+                 input_shape,
+                 n_classes,
+                 num_res_blocks=3,
+                 num_filters_in=16,
+                 dropout_rate=.3):
+        self.input_shape = input_shape
+        self.n_classes = n_classes
+
+        self.num_res_blocks = num_res_blocks
+        self.num_filters_in = num_filters_in
+        self.backbone = ResNetV2(num_res_blocks=self.num_res_blocks,
+                                 num_filters_in=self.num_filters_in)
+        self.dropout_rate = dropout_rate
+        self.dropout = Dropout(rate=dropout_rate)
+
+    def data_preprocess(self, inputs, training=True):
+        return inputs
+
+    def data_postprocess(self, outputs, meta):
+        return outputs
+
+    def build_model(self):
+        img_height, img_width, _ = self.input_shape
+        image_input = Input(shape=self.input_shape, name='resnet_input')
+        backbone = self.backbone.call(image_input)
+        x = backbone.layers[-1].output
+        x = self.dropout(x)
+        x = GlobalAveragePooling2D()(x)
+        cls = Dense(units=self.n_classes, activation='sigmoid')(x)
+        kpt_x = Dense(units=self.n_classes, activation='sigmoid')(x)
+        kpt_y = Dense(units=self.n_classes, activation='sigmoid')(x)
+        kpt = Concatenate(axis=-1)([kpt_x, kpt_y])
+        kpt = Reshape(target_shape=(-1, 2))(kpt)
+
+        predictions = {'cls': cls, 'kpt': kpt}
+        self.model = Model(image_input, predictions)
+
+        return self
+
+    def compile(self, optimizer=None, loss=None, metrics=None, loss_weights=None):
+        optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=5e-04) \
+            if optimizer is None else optimizer
+        loss = {
+            'cls': binary_crossentropy,
+            'kpt': mse
+        } if loss is None else loss
+
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics, loss_weights=loss_weights)
+
+        return self
